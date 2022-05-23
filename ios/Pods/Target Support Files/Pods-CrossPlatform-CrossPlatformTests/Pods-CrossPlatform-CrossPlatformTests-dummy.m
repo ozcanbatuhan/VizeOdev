b7f8cd8ac53fddc25168d817ff56f5d6f2b388ee
ozcanbatuhan <batuhan.ozcn@outlook.com>
2022-05-23 20:02:24 +0300

    ios

---
+++ ios/Pods/Target Support Files/Pods-CrossPlatform-CrossPlatformTests/Pods-CrossPlatform-CrossPlatformTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrossPlatform_CrossPlatformTests : NSObject
+@end
+@implementation PodsDummy_Pods_CrossPlatform_CrossPlatformTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-CrossPlatform/Pods-CrossPlatform-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrossPlatform : NSObject
+@end
+@implementation PodsDummy_Pods_CrossPlatform
+@end
