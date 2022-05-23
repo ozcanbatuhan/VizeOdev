5136879a09688a927624d9b19dc7385378ee4e5a
ozcanbatuhan <batuhan.ozcn@outlook.com>
2022-05-23 19:47:40 +0300

    batuhan_ogulcan

---
+++ App.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import Route from './src/Route';
+
+
+
+export default Route;
